8050e3a901f3074d2d3c6a3fad57c36be5881776
bedrocktothetop <122586019+bedrocktothetop@users.noreply.github.com>
2023-03-24 23:41:52 +0800

    Create week4_get_time.rs

---
+++ week4_get_time.rs
@@ -0,0 +1,30 @@
+enum TrafficLight {
+    Red,
+    Yellow,
+    Green,
+}
+
+trait Time {
+    fn get_time(&self) -> u32;
+}
+
+impl Time for TrafficLight {
+    fn get_time(&self) -> u32 {
+        match *self {
+            TrafficLight::Red => 60,
+            TrafficLight::Yellow => 5,
+            TrafficLight::Green => 60,
+        }
+    }
+}
+
+fn main() {
+    let red = TrafficLight::Red;
+    println!("Red light is {} seconds.", red.get_time());
+
+    let yellow = TrafficLight::Yellow;
+    println!("Yellow light is {} seconds.", yellow.get_time());
+
+    let green = TrafficLight::Green;
+    println!("Green light is {} seconds.", green.get_time());
+}
